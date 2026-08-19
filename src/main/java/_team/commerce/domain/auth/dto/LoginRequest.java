1f5d842c3152f8ebe9e45c4a70eb8f6ef4897d57
t2025-m0237 <asd@asad>
2026-08-19 17:41:24 +0900

    feat: 로그인 요청 필드 및 검증 추가

--- src/main/java/_team/commerce/domain/auth/dto/LoginRequest.java
+++ src/main/java/_team/commerce/domain/auth/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package _team.commerce.domain.auth.dto;
 
-public record LoginRequest() {
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+
+        @NotBlank(message = "이메일을 입력하세요")
+        String email,
+
+        @NotBlank(message = "비밀번호를 입력하세요")
+        String password
+) {
 }
