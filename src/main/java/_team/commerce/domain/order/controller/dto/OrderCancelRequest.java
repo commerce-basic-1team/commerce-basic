9c8903a3f7051751afb3736732c1a55ef117985c
JIMIN <bagj5082@gmail.com>
2026-08-20 19:25:03 +0900

    feat: 주문 취소-2

--- src/main/java/_team/commerce/domain/order/controller/dto/OrderCancelRequest.java
+++ src/main/java/_team/commerce/domain/order/controller/dto/OrderCancelRequest.java
@@ -1,5 +1,11 @@
 package _team.commerce.domain.order.controller.dto;
 
-public record OrderCancelRequest() {
+import jakarta.validation.constraints.NotBlank;
 
+public record OrderCancelRequest(
+
+        @NotBlank(message = "취소 사유는 필수입니다.")
+        String reason
+
+) {
 }
